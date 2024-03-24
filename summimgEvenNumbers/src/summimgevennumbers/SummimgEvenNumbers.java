5e3e056d8cd1bbebcb9b5d15cc5747d03470d5e3
Guegouo  Moghommahie Guiddel <125418589+Apache-ghost@users.noreply.github.com>
2024-03-24 10:25:19 +0100

    Update SummimgEvenNumbers.java

--- summimgEvenNumbers/src/summimgevennumbers/SummimgEvenNumbers.java
+++ summimgEvenNumbers/src/summimgevennumbers/SummimgEvenNumbers.java
@@ -33,3 +33,35 @@ public class SummimgEvenNumbers {
     }
     
 }
+
+
+// try this one, is more dynamic:
+    
+// import java.util.Scanner;
+
+// public class SummingEvenNumbers {
+//     public static void main(String[] args) {
+//         Scanner scan = new Scanner(System.in);
+//         int total = 0;
+//         int count = 0;
+        
+//         System.out.println("Enter numbers. Enter a negative number to stop.");
+
+//         while (true) {
+//             System.out.print("Enter a number: ");
+//             int number = scan.nextInt();
+            
+//             if (number < 0) {
+//                 break;
+//             }
+            
+//             if (number % 2 == 0) {
+//                 total += number;
+//                 count++;
+//             }
+//         }
+        
+//         System.out.println("Sum of even numbers: " + total);
+//         System.out.println("Count of even numbers: " + count);
+//     }
+// }
