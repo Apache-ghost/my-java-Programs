67a52e2231690feb8e14dfb344b173654e0add9c
Guegouo  Moghommahie Guiddel <125418589+Apache-ghost@users.noreply.github.com>
2024-04-04 14:03:54 +0200

    Update Factorial.java
    I Remove the unnecessary comments

--- factorial/src/factorial/Factorial.java
+++ factorial/src/factorial/Factorial.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package factorial;
 
-/**
- *
- * @author Administrator
- */
 import java.util.Scanner;
 public class Factorial {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
       Scanner scan=new Scanner(System.in);
        int factorial=1;
