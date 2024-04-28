4c11588830969a22e962139518894acacf44cb3e
Guegouo  Moghommahie Guiddel <125418589+Apache-ghost@users.noreply.github.com>
2024-04-28 06:14:36 +0200

    Update Factorial.java
    view the comments

--- factorial/src/factorial/Factorial.java
+++ factorial/src/factorial/Factorial.java
@@ -32,3 +32,20 @@ public class Factorial {
     }
     
 }
+// import java.util.Scanner;
+
+// public class Factorial {
+//     public static void main(String[] args) {
+//         Scanner scan = new Scanner(System.in);
+
+//         System.out.println("Enter the number for which you want to evaluate the factorial:");
+//         int num = scan.nextInt();
+//         int factorial = num < 0 ? -1 : 1;
+
+//         for (int i = 1; i <= num; i++) {
+//             factorial *= i;
+//         }
+
+//         System.out.println("The factorial of " + num + " is " + factorial);
+//     }
+// }
